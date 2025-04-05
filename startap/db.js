923e07c5bd95c63f6c5ee077669e57e1c620c4f0
MIRLAZIZ <mirlazizmirtolipov6gmail.com>
2025-04-05 22:56:47 +0500

    test

--- startap/db.js
+++ startap/db.js
@@ -6,7 +6,7 @@ const  config = require('config')
 module.exports = function () {
     // MongoDBga ulanish
     mongoose
-        .connect(config.get('db'))
+        .connect(process.env.MONGO_URI)
         .then(() => winston.info("MongoDb ga ulanish xosil qilindi"))
 
 }
