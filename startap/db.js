875e92ddfb5f65747c0a6da97bae8c662f6744bc
MIRLAZIZ <mirlazizmirtolipov6gmail.com>
2025-04-05 23:03:44 +0500

    url

--- startap/db.js
+++ startap/db.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 const winston = require('winston')
-const  config = require('config')
+const config = require('config')
 
 
 module.exports = function () {
     // MongoDBga ulanish
     mongoose
-        .connect(process.env.MONGO_URI)
+        .connect('mongodb://mongo:vXnygejYsAjoZEmBECBpHEmGmOwpksPN@mongodb.railway.internal:27017')
         .then(() => winston.info("MongoDb ga ulanish xosil qilindi"))
 
 }
